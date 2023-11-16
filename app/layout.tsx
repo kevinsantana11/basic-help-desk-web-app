41befec7bf0f723c89757e324b6e7c054d4c1591
Kevin Santana <kevinsantana11@gmail.com>
2023-11-16 12:48:34 -0500

    better separation of concerns

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import AuthButton from "../components/AuthButton";
-import { GeistSans } from "geist/font";
 import "./globals.css";
+import { cookies } from "next/headers";
+import { createClient } from "@/utils/supabase/server";
+import Link from "next/link";
+import { Home2 } from "@/components/icons";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -12,21 +15,67 @@ export const metadata = {
   description: "A simple ticketing request application",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+
+  const getUserSection = async () => {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+
+    let userControls = null;
+    if (user) {
+      const getUserRoleReq = await supabase
+        .from("users")
+        .select()
+        .eq("id", user.id)
+        .single();
+
+        if (!getUserRoleReq.error) {
+          userControls = (
+            <Link href="/?force=true">
+              <button className="rounded p-2 bg-slate-800">
+                Change Role (Current: {getUserRoleReq.data.role})
+              </button>
+            </Link>
+          );
+        }
+    }
+
+    return (
+        <div className="flex flex-row gap-x-5 items-baseline">
+          <div className="flex flex-row gap-x-1">
+            <AuthButton user={user}/>
+            {userControls}
+          </div>
+        </div>
+    );
+  }
+
   return (
-    <html lang="en" className={GeistSans.className}>
+    <html lang="en">
       <body className="bg-background text-foreground">
         <main className="min-h-screen flex flex-col items-center">
           <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-            <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-              <p className="text-blue-500">
-                <b>Basic Ticketing Application</b>
-              </p>
-              <AuthButton />
+            <div className="w-full max-w-6xl flex justify-between items-center p-3 text-sm">
+                <div className="flex flex-row gap-x-5 items-center">
+                  <Link href="/">
+                    <button className="p-2 rounded bg-slate-800">
+                      <Home2 />
+                    </button>
+                  </Link>
+                  <p className="text-blue-500">
+                    <b>Basic Ticketing Application</b>
+                  </p>
+                </div>
+                {await getUserSection()}
             </div>
           </nav>
           {children}
--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -3,15 +3,12 @@ import Link from "next/link";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { Home2 } from "./icons";
+import { User } from "@supabase/supabase-js";
 
-export default async function AuthButton() {
-  const cookieStore = cookies();
-  const supabase = createClient(cookieStore);
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
+interface AuthButtonProps {
+  user: User | null
+} 
+export default async function AuthButton({ user }: AuthButtonProps) {
   const signOut = async () => {
     "use server";
 
@@ -23,12 +20,6 @@ export default async function AuthButton() {
 
   return user ? (
     <div className="flex items-center gap-4">
-      <Link href="/?force=true">
-        <button className="rounded p-1 bg-slate-700">Change Role</button>
-      </Link>
-      <Link href="/">
-        <Home2 />
-      </Link>
       Hey, {user.email}!
       <form action={signOut}>
         <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
